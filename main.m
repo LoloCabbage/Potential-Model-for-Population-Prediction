71a25308ebd96c202591faae2ed0a606d318cead
LoloCabbage <108572718+LoloCabbage@users.noreply.github.com>
2023-07-06 18:10:34 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
 %% load dataset
 addpath("functions");
-dir_info = dir('urban_land');
+dir_info = dir('urban_land'); % urban land data 
 dir_info(1:2) = []; %remove the "."&".." FILE
-scenarios = readtable('urban_land.csv');
-BETA = readtable('betas.csv');
+scenarios = readtable('urban_land.csv'); % senario limitation
+BETA = readtable('betas.csv'); %beta
 
 %% run the model
 tic
